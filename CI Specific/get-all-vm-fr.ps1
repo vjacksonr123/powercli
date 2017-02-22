aa17f42d20d6f0e958a9450f211098a5c67a13eb
Reggie Jackson <rjackson@bofh.me>
2017-02-22 17:41:19 -0600

    simple script to get all vms
    simple script to get all vms

---
+++ CI Specific/get-all-vm-fr.ps1
@@ -0,0 +1,6 @@
+Connect-VIServer vm-vc-fr.cidom.careington.com
+
+Get-VM | Select Name, @{N="Cluster";E={Get-Cluster -VM $_}}, @{N="ESX Host";E={Get-VMHost -VM $_}}, @{N="Datastore";E={Get-Datastore -VM $_}} | Export-Csv -NoTypeInformation C:\Scripts\VM_CLuster_Host_Datastore.csv 
+
+
+Disconnect-viServer
